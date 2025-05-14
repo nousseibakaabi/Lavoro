d5eb8d7ab90234c2e5eddd9c77490380f6f32f3a
nousseibakaabi <nousseiba.kaabi@esprit.tn>
2025-05-14 23:13:16 +0100

    rewrite

--- LavoroBack/routes/skills.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const skillsController = require('../controllers/skillsController');
-
-router.get('/getSkills', skillsController.getAllSkills);
-
-module.exports = router;
--- LavoroBack/routes/userSkills.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userSkillsController = require('../controllers/userSkillsController');
-
-//router.post('/getSkillsByUserId', userSkillsController.getSkillsByUserId);
-router.post('/getSkillsForMultipleUsers', userSkillsController.getSkillsForMultipleUsers);
-router.post('/searchUsersBySkills', userSkillsController.searchUsersBySkills);
-
-module.exports = router;
